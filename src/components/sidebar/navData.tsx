bd98481648ee987c09c4b9869c970d93577e7edc
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-09-04 18:19:09 +0700

    created sidebar navigation

---
+++ src/components/sidebar/navData.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const NavData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'price-rules',
+    path: '/price-rules',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  }
+];
