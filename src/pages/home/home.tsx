068a30d4ceb65d093bdfeab1258bb256752283e7
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-09-14 21:00:45 +0700

    added homepage with welcome content

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './home.styles.css';
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <div className="container">
+          <h1>Добро пожаловать в систему CloneService!</h1>
+          <img src={process.env.PUBLIC_URL + 'site-logo.png'} className="logo" alt={'logo'} />
+        </div>
+
+        <div className="font_rectangles">
+          <div className="front_rectangles_item" />
+          <div className="front_rectangles_item" />
+          <div className="front_rectangles_item" />
+          <div className="front_rectangles_item" />
+          <div className="front_rectangles_item" />
+          <div className="front_rectangles_item" />
+        </div>
+      </div>
+    </>
+  );
+}
