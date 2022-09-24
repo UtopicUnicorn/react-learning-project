7718ad2746bb7ee7adb63196c71fd01aed4f1032
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-09-24 20:41:30 +0700

    added new route for partners module

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Sidebar from './components/sidebar/sidebar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import Index from './pages/price-rules';
+import Partners from './pages/patners';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/price-rules" element={<Index />} />
+          <Route path="/partners" element={<Partners />} />
         </Routes>
       </Router>
     </>
