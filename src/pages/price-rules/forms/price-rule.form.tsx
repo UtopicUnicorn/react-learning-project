57911480a44135626975cf09571324618287497c
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-11-19 15:32:10 +0700

    added price rule reducer and added library for table

--- src/pages/price-rules/forms/price-rule.form.tsx
+++ src/pages/price-rules/forms/price-rule.form.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import priceRuleService from '../services/price-rule.service';
 import '../price-rules.styles.css';
+import { TextField } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { priceRuleState } from '../price-rule.reducer';
 
 function PriceRuleForm() {
   const [name, setName] = React.useState('');
-  const [percent, setPercent] = React.useState('0');
-  const [minSum, setMinSum] = React.useState('0');
-  const [maxSum, setMaxSum] = React.useState('0');
+  const [percent, setPercent] = React.useState('');
+  const [minSum, setMinSum] = React.useState('');
+  const [maxSum, setMaxSum] = React.useState('');
+
+  const dispatch = useDispatch();
 
   const Check = () => {
     const obj = {
@@ -15,24 +20,52 @@ function PriceRuleForm() {
       min: Number(minSum),
       max: Number(maxSum)
     };
-
+    dispatch({ type: 'NEW_RULE', payload: obj });
     priceRuleService.post(obj);
   };
 
   return (
     <div style={{ textAlign: 'center' }}>
       <form>
-        <p>Название</p>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <TextField
+          style={{ margin: 10 }}
+          id="outlined-basic"
+          label="Название"
+          variant="filled"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <p>Процент</p>
-        <input type="text" value={percent} onChange={(e) => setPercent(e.target.value)} />
+        <TextField
+          style={{ margin: 10 }}
+          id="outlined-basic"
+          label="Процент"
+          variant="filled"
+          type="text"
+          value={percent}
+          onChange={(e) => setPercent(e.target.value)}
+        />
 
-        <p>Минимальная сумма</p>
-        <input type="text" value={minSum} onChange={(e) => setMinSum(e.target.value)} />
+        <TextField
+          style={{ margin: 10 }}
+          type="text"
+          id="outlined-basic"
+          label="Минимальная сумма"
+          variant="filled"
+          value={minSum}
+          onChange={(e) => setMinSum(e.target.value)}
+        />
 
-        <p>Максимальная сумма</p>
-        <input type="text" value={maxSum} onChange={(e) => setMaxSum(e.target.value)} />
+        <TextField
+          style={{ margin: 10 }}
+          type="text"
+          id="outlined-basic"
+          label="Максимальная сумма"
+          variant="filled"
+          value={maxSum}
+          onChange={(e) => setMaxSum(e.target.value)}
+        />
       </form>
 
       <button type={'submit'} onClick={Check}>
--- src/pages/price-rules/index.tsx
+++ src/pages/price-rules/index.tsx
@@ -6,15 +6,31 @@ import PriceRuleForm from './forms/price-rule.form';
 import PriceRuleService from './services/price-rule.service';
 
 import { PriceRuleInterface } from './interfaces/price-rule.interface';
+import { useSelector } from 'react-redux';
+import { priceRuleState } from './price-rule.reducer';
+import { DataGrid, GridColDef, GridColumns } from '@mui/x-data-grid';
 
-export default function Index() {
+export default function PriceRules() {
   const [modal, setModal] = useState(false);
   const [rules, setRules] = useState<PriceRuleInterface[]>([]);
 
   useEffect(() => getFunction, []);
 
+  const columns: GridColDef[] = [
+    { field: 'name', headerName: 'Имя', width: 130 },
+    { field: 'percent', headerName: 'Процент', width: 130 },
+    { field: 'min', headerName: 'Минимальная цена', width: 130 },
+    { field: 'max', headerName: 'Максимальная цена', width: 130 }
+  ];
+
   const getFunction = () => {
     PriceRuleService.get().then((obj) => setRules(obj.data));
+    addRule();
+  };
+  const newRule = useSelector((state: priceRuleState) => state);
+
+  const addRule = () => {
+    console.log(newRule);
   };
 
   const headers: { label: string }[] = [
@@ -26,36 +42,18 @@ export default function Index() {
   ];
 
   return (
-    <div className="container">
+    <div>
+      <h1>Ценовые правила</h1>
       <main>
-        <h1 className="title">Ценовые правила</h1>
-        <button className="button-add" onClick={() => setModal(true)}>
-          <img src={process.env.PUBLIC_URL + 'btn-plus.svg'} className="btn_plus" alt={'logo'} />
-          <a>Добавить</a>
-        </button>
-
-        <table>
-          <thead>
-            <tr>
-              {headers.map((row, index) => {
-                return <td key={index}>{row.label} </td>;
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {rules.map((rule) => {
-              return (
-                <tr key={rule.id}>
-                  <td>{rule.id}</td>
-                  <td>{rule.name}</td>
-                  <td>{rule.percent}</td>
-                  <td>{rule.min}</td>
-                  <td>{rule.max}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <div style={{ textAlign: 'center' }}>
+          <button className="button-add" onClick={() => setModal(true)}>
+            <img src={process.env.PUBLIC_URL + 'btn-plus.svg'} className="btn_plus" alt={'logo'} />
+            <a>Добавить</a>
+          </button>
+        </div>
+        <div className="table">
+          <DataGrid rows={rules} columns={columns} pageSize={5} rowsPerPageOptions={[5]} />
+        </div>
       </main>
       <ModalWindow active={modal} setActive={setModal}>
         <PriceRuleForm />
