913b4c2c169069c4656e6f6c396d7a26b75215e2
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-11-19 15:35:10 +0700

    added auth form for singIn and signUp

---
+++ src/pages/auth/reducers/auth.reducer.ts
@@ -0,0 +1,33 @@
+import { AnyAction } from 'redux';
+
+export const authInitialState = {
+  login: 'login',
+  password: 'password'
+};
+
+export type authState = ReturnType<typeof authReducer>;
+
+export const authReducer = (state = authInitialState, action: AnyAction) => {
+  switch (action.type) {
+    case 'NEW_USER': {
+      return {
+        ...state,
+        login: action.payload.login,
+        password: action.payload.password
+      };
+    }
+
+    //
+    case 'SIGN_IN': {
+      return {
+        ...state,
+        login: action.payload.login,
+        password: action.payload.password
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
---
+++ src/pages/auth/services/auth.service.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+import Axios from '../../../components/axios/axios-instance';
+import { UserInterface } from '../interfaces/user.interface';
+import { loginURL } from '../../../tmpURLs';
+
+export default {
+  login(user: UserInterface) {
+    return Axios.post(loginURL + 'check', user);
+  },
+
+  createUser(user: UserInterface) {
+    return Axios.post(loginURL, user);
+  }
+};
