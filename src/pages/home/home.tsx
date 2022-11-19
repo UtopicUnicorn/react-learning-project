e76ddd76cf78780f964a38da7ea073d4c59ebe57
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-11-19 16:00:30 +0700

    changed home styles

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,13 +4,10 @@ import './home.styles.css';
 
 export default function Home() {
   return (
-    <>
+    <div className="home_container">
       <div>
-        <div className="container">
-          <h1>Добро пожаловать в систему SomeCompany!</h1>
-          <img src={process.env.PUBLIC_URL + 'site-logo.png'} className="logo" alt={'logo'} />
-        </div>
-
+        <h1>Добро пожаловать в систему SomeCompany!</h1>
+        <img src={process.env.PUBLIC_URL + 'site-logo.png'} className="logo" alt={'logo'} />
         <div className="font_rectangles">
           <div className="front_rectangles_item" />
           <div className="front_rectangles_item" />
@@ -20,6 +17,6 @@ export default function Home() {
           <div className="front_rectangles_item" />
         </div>
       </div>
-    </>
+    </div>
   );
 }
