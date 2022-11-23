e2506bd1c5e4c4c78f0d135877b184f2d175d06d
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-11-23 20:37:43 +0700

    updated styles and layout

--- src/pages/price-rules/index.tsx
+++ src/pages/price-rules/index.tsx
@@ -33,24 +33,18 @@ export default function PriceRules() {
     console.log(newRule);
   };
 
-  const headers: { label: string }[] = [
-    { label: 'ID' },
-    { label: 'Название' },
-    { label: 'Процент' },
-    { label: 'Минимальная цена' },
-    { label: 'Максимальная цена' }
-  ];
-
   return (
     <div>
-      <h1>Ценовые правила</h1>
-      <main>
-        <div style={{ textAlign: 'center' }}>
+      <div className="test_head">
+        <h1>Ценовые правила</h1>
+        <div>
           <button className="button-add" onClick={() => setModal(true)}>
             <img src={process.env.PUBLIC_URL + 'btn-plus.svg'} className="btn_plus" alt={'logo'} />
             <a>Добавить</a>
           </button>
         </div>
+      </div>
+      <main>
         <div className="table">
           <DataGrid rows={rules} columns={columns} pageSize={5} rowsPerPageOptions={[5]} />
         </div>
