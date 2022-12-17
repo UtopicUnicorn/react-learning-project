3ed9deff0d00b7f272b33985840e98a4b7a6c97c
UtopicUnicorn <medvedevtimofei@gmail.com>
2022-12-17 13:35:35 +0700

    changed auth check to look into localstorage

--- src/components/hooks/auth-hook.ts
+++ src/components/hooks/auth-hook.ts
@@ -1,8 +1,5 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '../../reducer';
-
 export const useAuth = () => {
-  const { login } = useSelector((state: RootState) => state.auth);
+  const login = localStorage.getItem('key');
   return {
     isAuth: !!login
   };
